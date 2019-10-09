032b3c8f193b974bb6199a52c4da967567540a18
itsdrah <50168419+itsdrah@users.noreply.github.com>
2019-10-09 21:20:58 +0200

    Adding the gameState enum to the code as well as the matrixObject struct.

--- Minesweeper/Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper/Minesweeper.cpp
@@ -1,20 +1,25 @@
-// Minesweeper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+enum gameState 
+{
+	GAME_LOST = -1,
+	GAME_IN_PROGRESS = 0,
+	GAME_WON = 1
+	// Should we also add GAME_START?
+};
+
+struct matrixObject
+{
+	int objType;
+	bool isVisible, isFlagged;
+};
+
+// For demonstration purposes only
+int actualGameState = 0;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << ((actualGameState == 0) ? "The game is in progress...\n" : "The game has not started yet, or it has already ended...\n");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
