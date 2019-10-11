2fa4fd93de97fdebd7cfbfd431da9f0fc2af9231
itsdrah <50168419+itsdrah@users.noreply.github.com>
2019-10-11 13:50:29 +0200

    Added global variable actualGameState, intialized as GAME_SETUP.

--- Minesweeper/Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper/Minesweeper.cpp
@@ -13,12 +13,11 @@ enum gameState
 struct matrixObject
 {
 	int objType; // Value can range from -1 to 8. 0-8 then its a field (the number depends on how many mines there are around the field), -1 if its a mine.
-	bool isVisible; // This variable decides if the field is rendered depending on objType or as an X. Also decides if the field can be poked or not in the function pokeField().
 	bool isFlagged; // This variable decides if the field is rendered as an F or X. Also decides if the field can be poked or not in the function pokeField().
+	bool isVisible; // This variable decides if the field is rendered depending on objType or as an X. Also decides if the field can be poked or not in the function pokeField().
 };
 
-// For demonstration purposes only
-int actualGameState = GAME_IN_PROGRESS;
+int actualGameState = GAME_SETUP;
 
 int main()
 {
