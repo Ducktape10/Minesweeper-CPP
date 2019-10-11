4b2bb06fa46d58e4b73e129129f90431d80a037f
Pal Farkas <pal.farkas7@gmail.com>
2019-10-11 19:14:06 +0200

    remove debug from generatemap

--- Minesweeper/Minesweeper/generateMap.cpp
+++ Minesweeper/Minesweeper/generateMap.cpp
@@ -1,6 +1,5 @@
 #include "MatrixObject.h"
 #include "getNearFields.h"
-#include <iostream>
 
 matrixObject **generateMap(matrixObject **minesMatrix,int width, int heigth) {
 	for (int x = 0; x < width; x++) {
@@ -8,13 +7,10 @@ matrixObject **generateMap(matrixObject **minesMatrix,int width, int heigth) {
 			if (minesMatrix[x][y].objType != -1) {
 				int** nearFields = getNearFields(x, y, width, heigth);
 				int fieldsLength = getLengthOfFieldPointer(x, y, width, heigth);
-				//std::cout << fieldsLength << " ";
 				int mines = 0;
 				for (int i = 0; i < fieldsLength; i++) {
 					int fX = nearFields[i][0];
 					int fY = nearFields[i][1];
-					std::cout << fX << " " << fY << std::endl;
-					std::cout << minesMatrix[fX][fY].objType << std::endl;
 					if (minesMatrix[fX][fY].objType == -1) {
 						mines++;
 					}
@@ -22,7 +18,6 @@ matrixObject **generateMap(matrixObject **minesMatrix,int width, int heigth) {
 				minesMatrix[x][y].objType = mines;
 			}
 		}
-		//std::cout << std::endl;
 	}
 
 	return minesMatrix; // You can define the size of matrixObject when you call this function
