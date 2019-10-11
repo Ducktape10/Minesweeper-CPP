13f5ef24bba265db4655135a407eed24cb30a962
itsdrah <50168419+itsdrah@users.noreply.github.com>
2019-10-11 13:33:54 +0200

    Documentation for the matrixObject struct. Also added GAME_SETUP to the gameState enum.

--- Minesweeper/Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper/Minesweeper.cpp
@@ -5,15 +5,16 @@ using namespace std;
 enum gameState 
 {
 	GAME_LOST = -1,
-	GAME_IN_PROGRESS = 0,
-	GAME_WON = 1
-	// Should we also add GAME_START?
+	GAME_SETUP,
+	GAME_IN_PROGRESS,
+	GAME_WON
 };
 
 struct matrixObject
 {
-	int objType;
-	bool isVisible, isFlagged;
+	int objType; // Value can range from -1 to 8. 0-8 then its a field (the number depends on how many mines there are around the field), -1 if its a mine.
+	bool isVisible; // This variable decides if the field is rendered depending on objType or as an X. Also decides if the field can be poked or not in the function pokeField().
+	bool isFlagged; // This variable decides if the field is rendered as an F or X. Also decides if the field can be poked or not in the function pokeField().
 };
 
 // For demonstration purposes only
@@ -21,5 +22,5 @@ int actualGameState = GAME_IN_PROGRESS;
 
 int main()
 {
-    cout << ((actualGameState == 0) ? "The game is in progress...\n" : "The game has not started yet, or it has already ended...\n");
+    cout << ((actualGameState == GAME_IN_PROGRESS) ? "The game is in progress...\n" : "The game has not started yet, or it has already ended...\n");
 }
