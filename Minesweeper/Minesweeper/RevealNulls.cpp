3a6db582176e7605da63241bafe846e775ba78a6
Fekete Márk <fmark98@gmail.com>
2019-10-15 20:45:18 +0200

    Uses a fuction in RecealNulls.cpp that I didn't noticed before.

--- Minesweeper/Minesweeper/RevealNulls.cpp
+++ Minesweeper/Minesweeper/RevealNulls.cpp
@@ -7,7 +7,7 @@ void revealNulls(int x, int y, matrixObject** matrix, int width, int heigth)
 {
 	matrix[x][y].isVisible = true;
 	int** nearFields = getNearFields(x, y, width, heigth);
-	for (int i = 0; i < sizeof(**nearFields) / sizeof(int[2]); ++i)
+	for (int i = 0; i < getLengthOfFieldPointer(x, y, width, heigth); ++i)
 	{
 		if (!matrix[nearFields[i][0]][nearFields[i][1]].isVisible)
 		{
