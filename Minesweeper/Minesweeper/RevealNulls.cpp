381484bfcdd1a6f5f264b00f0c405bdef507987d
Fekete Márk <fmark98@gmail.com>
2019-10-15 20:10:21 +0200

    RevealNulls.cpp created, ready for tests.

---
+++ Minesweeper/Minesweeper/RevealNulls.cpp
@@ -0,0 +1,17 @@
+#include "MatrixObject.h"
+#include "getNearFields.h"
+
+using namespace std;
+
+void revealNulls(int x, int y, matrixObject** matrix, int width, int heigth)
+{
+	matrix[x][y].isVisible = true;
+	int** nearFields = getNearFields(x, y, width, heigth);
+	for (int i = 0; i < sizeof(**nearFields) / sizeof(int[2]); ++i)
+	{
+		if (!matrix[nearFields[i][0]][nearFields[i][1]].isVisible)
+		{
+			revealNulls(nearFields[i][0], nearFields[i][1], matrix, width, heigth);
+		}
+	}
+}
